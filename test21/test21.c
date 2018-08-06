eec09f79a7dfa88c3de5b8a95fd4f609b1919058
liuchao <1176205403@qq.com>
2018-08-06 20:10:22 +0800

    xiugai

---
+++ test21/test21.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void Swap(int *pa, int *pb)
+{
+	int tmp = *pa;
+	*pa = *pb;
+	*pb = tmp;
+}
+int main()
+{
+	int a = 10;
+	int b = 20;
+	printf("a=%d b=%d\n", a, b);
+	Swap(&a, &b);
+		printf("a=%d b=%d\n", a, b);
+	return 0;
+}
